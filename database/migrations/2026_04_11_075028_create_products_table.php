4f21e70c8db300d8770f375b3a7e218fa6a6495e
peterelauda <peterelauda>
2026-04-21 10:45:19 +0800

    DB Update for Product and Transaction in Multi-user Method

--- database/migrations/2026_04_11_075028_create_products_table.php
+++ database/migrations/2026_04_11_075028_create_products_table.php
@@ -13,7 +13,10 @@ return new class extends Migration {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('barcode')->unique()->nullable();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('product_code');
+            $table->unique(['user_id', 'product_code']);
+            $table->string('barcode')->nullable();
             $table->decimal('price', 12, 2);
             $table->integer('stock')->default(0);
             $table->timestamps();
--- database/migrations/2026_04_11_075127_create_transactions_table.php
+++ database/migrations/2026_04_11_075127_create_transactions_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('invoice_number');
+            $table->decimal('total', 12, 2);
+            $table->decimal('paid_amount', 12, 2);
+            $table->decimal('change_amount', 12, 2);
+            $table->string('payment_method');
             $table->timestamps();
         });
     }
